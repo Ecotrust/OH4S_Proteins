4db07bbd17f57f43b06a64a509a6c0179e4d8e70
Ryan Hodges <ryan.d.hodges@gmail.com>
2019-01-30 09:56:47 -0800

    moving bootstrap code into codebase for dist

--- app/portal/portal/settings.py
+++ app/portal/portal/settings.py
@@ -19,6 +19,14 @@ MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
+STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
