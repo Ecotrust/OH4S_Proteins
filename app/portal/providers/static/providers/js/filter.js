997e6082fa5410c3cbf2f6bd0dbacbabf03e1dd3
Ryan Hodges <ryan.d.hodges@gmail.com>
2019-02-05 17:34:24 -0800

    placeholder js

---
+++ app/portal/providers/static/providers/js/filter.js
@@ -0,0 +1,3 @@
+// TODO: Fire off AJAX request to backend to run filter.
+//    Callback takes HTML results and replaces $('#product-results-list') content
+$('#filter-form').on('change', function() { console.log($('#filter-form').serialize());});
