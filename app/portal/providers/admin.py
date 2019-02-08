4230b8283639f6597d169e904c5391f10a04cbb7
Ryan Hodges <ryan.d.hodges@gmail.com>
2019-02-08 13:32:10 -0800

    creating initial admin provider form

--- app/portal/providers/admin.py
+++ app/portal/providers/admin.py
@@ -2,8 +2,112 @@ from django.contrib import admin
 
 from .models import PoliticalRegion, PoliticalSubregion, DeliveryMethod, DeliveryMethod, Distributor, Provider, CapacityMeasurement, ProductCategory, Product, ProviderProduct, Identity, ProductionPractice
 
+# class ProductInline(admin.StackedInline):
+class ProductInline(admin.TabularInline):
+    model = ProviderProduct
+    fieldsets = (
+        (None, {
+            'fields': (
+                'name',
+                'category',
+                'description',
+            )
+        }),
+        ('Liability', {
+            'fields': (
+                ('productLiabilityInsurance', 'productLiabilityInsuranceAmount',),
+            )
+        }),
+        ('Availability', {
+            'fields': (
+                'deliveryMethods',
+                'regionalAvailability',
+                ('orderMinimum', 'deliveryMinimum',),
+                'distributors',
+            )
+        }),
+        ('Additional Info', {
+            'fields': (
+                'productionPractices',
+                ('capacityValue', 'capacityMeasurement',),
+                'image',
+                'notes',
+            )
+        }),
+
+    )
+
 class ProviderAdmin(admin.ModelAdmin):
     filter_horizontal = ('identities', 'deliveryMethods', 'regionalAvailability', 'distributors', 'productionPractices')
+
+    list_display = ('name','outreachConductor','businessAddressCity',
+    'businessAddressState','soldToSchoolsBefore','dateInfoUpdated')
+    search_fields = ('name','outreachConductor','businessAddressCity',
+    'businessAddressState','soldToSchoolsBefore','dateInfoUpdated')
+    readonly_fields = ('dateInfoUpdated'),
+
+    inlines = [
+        ProductInline,
+    ]
+
+    fieldsets = (
+        (None, {
+            # 'classes': ('producer-name',),
+            'fields': (
+                'name',
+                ('outreachConductor', 'dateInfoUpdated',),
+                'soldToSchoolsBefore',
+                'description',
+                ('primaryContactFirstName', 'primaryContactLastName',),
+            )
+        }),
+        ('Physical Address', {
+            'fields': (
+                'physicalAddressIsSame',
+                'physicalAddressLine1',
+                'physicalAddressLine2',
+                ('physicalAddressCity', 'physicalAddressState', 'physicalAddressZipCode',),
+            )
+        }),
+        ('Business Address', {
+            'fields': (
+                'businessAddressLine1',
+                'businessAddressLine2',
+                ('businessAddressCity', 'businessAddressState', 'businessAddressZipCode',),
+            )
+        }),
+        ('Contact Info', {
+            'fields': (
+                ('officePhone', 'cellPhone',),
+                'email',
+                'websiteAddress',
+            )
+        }),
+        ('Liability', {
+            'fields': (
+                ('productLiabilityInsurance', 'productLiabilityInsuranceAmount',),
+            )
+        }),
+        ('Availability', {
+            'fields': (
+                'deliveryMethods',
+                'regionalAvailability',
+                ('orderMinimum', 'deliveryMinimum',),
+                'distributors',
+            )
+        }),
+        ('Additional Info', {
+            'fields': (
+                'productionPractices',
+                'identities',
+                'notes',
+            )
+        }),
+    )
+
+    # add_form_template = '%s/TEKDB/templates/admin/CitationsForm.html' % BASE_DIR
+    # change_form_template = '%s/TEKDB/templates/admin/CitationsForm.html' % BASE_DIR
+
 
 admin.site.register(PoliticalRegion)
 admin.site.register(PoliticalSubregion)
