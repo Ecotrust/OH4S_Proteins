e0f41c0398fc1283b225a8225f9a56daa0e16ccd
Ryan Hodges <ryan.d.hodges@gmail.com>
2021-04-15 14:08:37 -0700

    homepage image now controlled via Wagtail

--- app/portal/cms/models.py
+++ app/portal/cms/models.py
@@ -1,16 +1,27 @@
 from django.db import models
+from django.conf import settings
 
 from wagtail.core.models import Page
 from wagtail.core.fields import RichTextField
 from wagtail.admin.edit_handlers import FieldPanel
+from wagtail.images.models import Image, AbstractImage, AbstractRendition
+from wagtail.images.edit_handlers import ImageChooserPanel
 
 from providers.views import header as get_header_context, get_category_context
 
 class HomePage(Page):
-    body = RichTextField(blank=True)
+    welcome = RichTextField(blank=True)
+    image = models.ForeignKey(
+        Image,
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='+'
+    )
 
     content_panels = Page.content_panels + [
-        FieldPanel('body', classname="full"),
+        FieldPanel('welcome', classname="full"),
+        ImageChooserPanel('image'),
     ]
 
     def get_context(self, request):
@@ -18,4 +29,8 @@ class HomePage(Page):
         context = super().get_context(request)
         context = get_header_context(request, context)
         context = get_category_context(request, context)
+        if self.image == None:
+            context['image'] = settings.DEFAULT_PROJECT_IMAGE
+        else:
+            context['image'] = self.image.file.url
         return context
