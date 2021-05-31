61670f875bc4569ffa798025c7ec39de51781b92
Ryan Hodges <ryan.d.hodges@gmail.com>
2021-05-31 14:11:54 -0700

    support for collecting filters from home and passing to results

---
+++ app/portal/providers/static/providers/js/home.js
@@ -0,0 +1,63 @@
+$(document).ready(function(){
+  $("form.home-filter-form").change(function(){
+    toggle_home_filter($(this));
+  });
+});
+
+var filters = {};
+// Observing changes to filters following Elliot B.'s advice:
+//  https://stackoverflow.com/a/50862441/706797
+var filterProxy = new Proxy(filters, {
+  set: function(target, key, value) {
+    console.log('${key} set to ${value}');
+    target[key] = value;
+    reset_filter_button();
+    return true;
+  }
+});
+
+var reset_filter_button = function() {
+  keys = Object.keys(filters);
+  filter_all = true;
+  for (var i = 0; i < keys.length; i++) {
+    key = keys[i];
+    if (filters[key].length > 0) {
+      filter_all = false;
+      break;
+    }
+  }
+  if (filter_all) {
+    $('#home-filter-button').html('Browse All Providers &gt;&gt;');
+  } else {
+    $('#home-filter-button').html('See Filtered Providers &gt;&gt;');
+  }
+}
+
+var toggle_home_filter = function(form) {
+  console.log('home filter change');
+  var checked = [];
+  var inputs = form.find('input');
+  for (var i = 0; i < inputs.length; i++) {
+    input = inputs[i];
+    if (input.checked) {
+      checked.push(parseInt(input.value));
+    }
+  }
+  filterProxy[form.attr('name')] = checked;
+}
+
+var go_to_results = function() {
+  var form = $('<form id="final-home-filter-form"></form>');
+  form.attr("method", "post");
+  form.attr("action", "/results/");
+  $.each( $('form.home-filter-form'), function(filter_index) {
+    var labels = $(this).find('label');
+    for (var i = 0; i < labels.length; i++) {
+      label = labels[i];
+      form.append(label);
+    }
+
+  });
+  form.appendTo('body').submit();
+
+}
