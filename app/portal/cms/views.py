a7ac89c77f0bbde145d83ca3e93b24f5d3943c6e
Ryan Hodges <ryan.d.hodges@gmail.com>
2021-06-14 16:46:52 -0700

    footer created

--- app/portal/cms/views.py
+++ app/portal/cms/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+def get_footer_context(request, context):
+    from cms.models import FooterPage
+    footer = FooterPage.objects.all()[0]
+    footer_context = footer.get_context(request)
+    for key in footer_context.keys():
+        context[key] = footer_context[key]
+    return context
