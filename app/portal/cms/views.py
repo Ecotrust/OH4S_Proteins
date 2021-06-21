2fd2237133660fda90981d087c20cc6cc532e156
Ryan Hodges <ryan.d.hodges@gmail.com>
2021-06-21 14:36:18 -0700

    Drive header content from Wagtail Snippets

--- app/portal/cms/views.py
+++ app/portal/cms/views.py
@@ -11,3 +11,17 @@ def get_footer_context(request, context):
         else:
             context['footer_page'] = footer_context['page']
     return context
+
+def get_header_context(request, context):
+    from cms.models import Header
+    headers = Header.objects.all()
+    if headers.count() == 0:
+        title = 'Oregon Harvest for Schools Directory'
+        image = 'providers/img/defaults/logo-oh4s-large.jpg'
+    else:
+        title = headers[0].title
+        image = headers[0].image
+
+    context['header_title'] = title
+    context['header_image'] = image
+    return context
